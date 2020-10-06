bf9b77493e3756094b8b8e9a7333e838a7d6fc79
TanyaMaximova1 <72431122+TanyaMaximova1@users.noreply.github.com>
2020-10-06 10:49:53 +0300

    Update Form1.cs

--- laba2/laba2/Form1.cs
+++ laba2/laba2/Form1.cs
@@ -16,10 +16,30 @@ namespace laba2
         {
             InitializeComponent();
         }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        
+         private void button1_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = "Война и мир";
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Лев Толстой";
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "2013 год";
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Азбука";
+        }
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1504";
         }
     }
 }
